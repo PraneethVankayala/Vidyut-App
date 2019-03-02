288bb3aec8ca5a2f95a16048bd888a5d96c9f3c7
Harshith <n.ravikumar537@gmail.com>
2019-03-02 17:35:04 +0530

    VidyutApp

--- app/src/main/java/com/example/vidyut/BottomNavigation/About.java
+++ app/src/main/java/com/example/vidyut/BottomNavigation/About.java
@@ -9,11 +9,11 @@ import android.view.ViewGroup;
 
 import com.example.vidyut.R;
 
-public class Wallet  extends Fragment {
+public class About extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_wallet,null);
+        return inflater.inflate(R.layout.fragment_about,null);
     }
 }
