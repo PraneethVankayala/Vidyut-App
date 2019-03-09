b1131fcd1eff4cbfab427784c3b288e86a24562b
PraneethVankayala <bspraneethvankayala@gmail.com>
2019-03-10 01:48:25 +0530

    Vidyut App

--- app/src/main/java/com/example/vidyut/MainActivity.java
+++ app/src/main/java/com/example/vidyut/MainActivity.java
@@ -3,16 +3,22 @@ package com.example.vidyut;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
+import android.graphics.Typeface;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
 import com.example.vidyut.BottomNavigation.About;
 import com.example.vidyut.BottomNavigation.Dashboard;
 import com.example.vidyut.BottomNavigation.Home;
@@ -26,18 +32,30 @@ import com.google.android.gms.tasks.Task;
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
     GoogleSignInClient mGoogleSignInClient;
     GoogleSignInOptions gso;
+    Typeface typeface;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        ActionBar ab = getSupportActionBar();
-//        ColorDrawable colorDrawable = new ColorDrawable(Color.parseColor("#ffffff"));
-//        ab.setBackgroundDrawable(colorDrawable);
+        int titleId = getResources().getIdentifier("action_bar_title", "id", "android");
+        TextView textView=new TextView(getApplicationContext());
+                typeface = ResourcesCompat.getFont(this, R.font.frontage_bold);
+                textView.setTypeface(typeface);
+                textView.setText("VIDYUT");
+                textView.setTextColor(getResources().getColor(R.color.colorPrimary));
+                textView.setGravity(Gravity.CENTER);
+                textView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+
+       ActionBar ab = getSupportActionBar();
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(textView);
+        ColorDrawable colorDrawable = new ColorDrawable(Color.parseColor("#ffffff"));
+        ab.setBackgroundDrawable(colorDrawable);
           getSupportActionBar().setDisplayShowTitleEnabled(false);
-//        final Drawable upArrow = getResources().getDrawable(R.drawable.abc_ic_ab_back_material);
-//        upArrow.setColorFilter(getResources().getColor(R.color.lightBlack), PorterDuff.Mode.SRC_ATOP);
-//        getSupportActionBar().setHomeAsUpIndicator(upArrow);
+        final Drawable upArrow = getResources().getDrawable(R.drawable.abc_ic_ab_back_material);
+        upArrow.setColorFilter(getResources().getColor(R.color.lightBlack), PorterDuff.Mode.SRC_ATOP);
+        getSupportActionBar().setHomeAsUpIndicator(upArrow);
         gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
                 .build();
